ce9b4ccd42aa8ae02cf5c18b14b931f305adf348
Mateusz Kopeć <mateuszkp96@gmail.com>
2020-03-20 00:10:30 +0100

    Spring Boot Application with simple config (#3)

---
+++ app/core/src/main/java/com/teamg/tourdeshot/TourdeshotApplication.java
@@ -0,0 +1,13 @@
+package com.teamg.tourdeshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourdeshotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourdeshotApplication.class, args);
+    }
+
+}
---
+++ app/core/src/test/java/com/teamg/tourdeshot/TourdeshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamg.tourdeshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourdeshotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
