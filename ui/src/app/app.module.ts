341e3edde05e4b95d1b1a2ad5b5589e86d45eee8
boxi01 <joannapudelko95@gmail.com>
2020-03-26 10:56:21 +0100

    Adding bootstrap and style for home page

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,30 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AppRoutingModule, routingComponents} from './app-routing.module';
+import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import {RouterModule, Routes} from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes} from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './footer/contact/contact.component';
 import { PrivacyComponent } from './footer/privacy/privacy.component';
 import { TeamComponent } from './footer/team/team.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-   routingComponents,
-   RegisterComponent,
-   HomeComponent,
-   ContactComponent,
-   PrivacyComponent,
-   TeamComponent
+    FooterComponent,
+    routingComponents,
+    RegisterComponent,
+    HomeComponent,
+    ContactComponent,
+    PrivacyComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
