f560d2232970d53b827f46c6aa2042c8767ca829
boxi01 <45149847+boxi01@users.noreply.github.com>
2020-03-30 16:27:45 +0200

    Integrate sigin module with dashboard (#17)

--- ui/src/app/register/register.component.ts
+++ ui/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService, SocialUser } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
 
 @Component({
   selector: 'app-register',
@@ -9,13 +11,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
+  private user: SocialUser;
+  private loggedIn: boolean;
+
   constructor(
     private router: Router,
-
+    private authService: AuthService,
   ) {
+
   }
 
-  ngOnInit(): void {
+  signInWithGoogle(): void {
+    console.log("loguje sie");
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
+      (res) => {
+        console.log("U are logged in!");
+        this.router.navigate(['signin']);
+      });
+      console.log('logged')
+  }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+      this.loggedIn = (user != null);
+    });
   }
 
 }
