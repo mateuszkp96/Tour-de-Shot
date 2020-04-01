f65231c465896ad9991f4f59337a7946071a736f
nowpawel96 <inpgnowpawel@gmail.com>
2020-04-01 20:33:06 +0200

    Created user microservice with basic security setup (#25)

---
+++ app/user/src/test/java/com/teamg/tourdeshot/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamg.tourdeshot.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
