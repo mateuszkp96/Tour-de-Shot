a81471f6bf4a9587b979d9c993cd422fccfdbf69
Mateusz Kopeć <mateuszkp96@gmail.com>
2020-04-04 10:16:39 +0200

    Core module test with embedded mongo and dev scripts repairing (#27)

--- app/core/devrun.sh
+++ app/core/devrun.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run -p 8080:8080 -t tourdeshot/core
+mongoContainerName="devmongo"
+mongoContainerPort="27017"
+port_mapping="8080:8080"
+mongoNetwork="core_network"
+
+docker run --rm \
+  -p "${port_mapping}" \
+  --network="${mongoNetwork}" \
+  -e SPRING_DATA_MONGODB_HOST="${mongoContainerName}" \
+  -e SPRING_DATA_MONGODB_PORT="${mongoContainerPort}" \
+  -t tourdeshot/core
