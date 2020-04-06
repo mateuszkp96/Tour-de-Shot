1cb541d5990e5ba2356c0260c8e040e97e01314a
boxi01 <45149847+boxi01@users.noreply.github.com>
2020-04-06 20:20:41 +0200

    Adding route page (#30)

--- ui/src/app/register/register.component.ts
+++ ui/src/app/register/register.component.ts
@@ -27,7 +27,7 @@ export class RegisterComponent implements OnInit {
       (res) => {
         console.log("U are logged in!");
         this.loggedIn = true;
-        this.router.navigate(['signin']);
+        this.router.navigate(['signin/search']);
       });
       console.log('logged')
   }
