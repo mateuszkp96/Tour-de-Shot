62f662e2cc7b545a6bd9a204400fe1a9d728951c
olabudzyn <ola.budz96@gmail.com>
2020-04-14 19:14:48 +0200

    added LocalRepositoryProxy which implements MenuRepository and ProductRepository interfaces (#42)

---
+++ app/core/src/main/java/com/teamg/tourdeshot/core/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.teamg.tourdeshot.core.repository;
+
+import com.teamg.tourdeshot.core.model.Local;
+import com.teamg.tourdeshot.core.model.Product;
+
+public interface ProductRepository {
+
+    Local addProductToLocal(Product product, Long localId);
+}
