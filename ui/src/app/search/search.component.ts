978cb240c9659768f9c2d71f0ccdd0b731d0d818
azwierzc <51276581+azwierzc@users.noreply.github.com>
2020-04-18 15:10:39 +0200

    Filter locals by radius (#50)
    * Filter locals by radius
    
    * Filter locals by radius v2

--- ui/src/app/search/search.component.ts
+++ ui/src/app/search/search.component.ts
@@ -29,12 +29,20 @@ export class SearchComponent implements OnInit, AfterViewInit {
 
   public local: Local;
   public localsList: Local[];
+  public filteredByDistLocalsList: Local[] = [];
   public checkedLocalsIdList: number[] = [];
   public pageSize = 3;
   public pageNumber = 2;
+  public radius: number;
+  place: google.maps.places.PlaceResult;
+  public localsCoordinates: google.maps.LatLng[] = [];
+  startPoint: google.maps.LatLng;
 
   private checkedLocalsIdListSource = new BehaviorSubject<number[]>([]);
   currentCheckedLocalsIdList = this.checkedLocalsIdListSource.asObservable();
+  private filteredByDistLocalsListSource = new BehaviorSubject<Local[]>([]);
+  currentFilteredByDistLocalsList = this.filteredByDistLocalsListSource.asObservable();
+
 
   constructor(
     private router: Router,
@@ -43,8 +51,8 @@ export class SearchComponent implements OnInit, AfterViewInit {
     public dialog: MatDialog,
     private mapsAPILoader: MapsAPILoader,
     private ngZone: NgZone
-  ) {
-  }
+  ) {}
+
 
   ngOnInit(): void {
     this.getLocalsList();
@@ -53,10 +61,27 @@ export class SearchComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     console.log("Search" + this.local)
 
-    this.autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {types: ["address"]});
-    this.autocomplete.addListener("place_changed", () => {
-      console.log(this.autocomplete);
+    this.autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+      types: ["address"],
+      componentRestrictions: {country: 'pl'}
     });
+
+    this.autocomplete.addListener("place_changed", () => {
+
+      this.ngZone.run(() => {
+        this.place = this.autocomplete.getPlace();
+
+        if (this.place.geometry === undefined || this.place.geometry === null) {
+          return;
+        }
+
+        this.startPoint = this.place.geometry.location;
+        console.log("start point changed")
+        console.log(this.startPoint)
+      });
+    });
+
+
   }
 
 
@@ -78,15 +103,36 @@ export class SearchComponent implements OnInit, AfterViewInit {
     });
   }
 
+
   getLocalById(id: number) {
     this.webLocalService.get().subscribe(data => {
       this.local = data[id] as Local;
     });
   }
 
+  filterLocalsByDist(radius: number) {
+    if (this.localsList) {
+      this.filteredByDistLocalsList = [];
+      this.localsList.forEach(element => {
+        let localCoordinates = new google.maps.LatLng(element.coordinates.lat, element.coordinates.long);
+        this.localsCoordinates.push(localCoordinates);
+        const distanceInKm = google.maps.geometry.spherical.computeDistanceBetween(localCoordinates, this.startPoint) / 1000;
+
+        if (distanceInKm < radius && (!this.filteredByDistLocalsList.includes(element))) {
+          this.filteredByDistLocalsList.push(element);
+        }
+      });
+    }
+  }
 
   onBtnSearchClicked() {
-    this.btnSearchClicked.next(this.autocomplete)
+    this.btnSearchClicked.next(this.startPoint)
+
+    this.filterLocalsByDist(this.radius);
+    console.log("filteredLocalsList")
+    console.log(this.filteredByDistLocalsList)
+    this.filteredByDistLocalsListSource.next(this.filteredByDistLocalsList);
+    this.checkedLocalsIdList = [];
   }
 
 
@@ -94,22 +140,42 @@ export class SearchComponent implements OnInit, AfterViewInit {
     switch (event.checked) {
       case true:
         local.checked = true;
-        //document.getElementById("listItem" + i).classList.add("checked");
+        // document.getElementById("listItem" + i).classList.add("checked");
         this.checkedLocalsIdList.push(local.id);
         this.checkedLocalsIdListSource.next(this.checkedLocalsIdList);
         break;
 
       case false:
         local.checked = false;
-        //document.getElementById("listItem" + i).classList.remove("checked");
-        const index =  this.checkedLocalsIdList.indexOf(local.id, 0);
-        this.checkedLocalsIdList.splice(index,1);
+        // document.getElementById("listItem" + i).classList.remove("checked");
+        const index = this.checkedLocalsIdList.indexOf(local.id, 0);
+        this.checkedLocalsIdList.splice(index, 1);
         this.checkedLocalsIdListSource.next(this.checkedLocalsIdList);
         break;
     }
-
   }
 
+  getChecked(local) {
+    for (let i of this.checkedLocalsIdList) {
+      if (local.id == i) {
+        return true;
+        continue;
+      }
+    }
+  }
+
+  getBackgroundColor(local) {
+    for (let i of this.checkedLocalsIdList) {
+      if (local.id == i) {
+        return '#dbdeeb';
+        continue;
+      }
+    }
+  }
+
+  onRadiusChanged(event) {
+    this.radius = event.currentTarget.valueOf().value.replace(/\D/g, '');
+  }
 
 
 }
