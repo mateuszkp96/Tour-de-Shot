bb29146d5d74929f30dbdffd711f35cf9512d056
Mateusz Kopeć <mateuszkp96@gmail.com>
2020-04-20 20:17:53 +0200

    Reading endpoints via environment variables for docker (#51)

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { AuthService, SocialUser } from "angularx-social-login";
+import { environment } from "../../environments/environment"
 
 @Component({
   selector: 'app-home',
@@ -20,6 +21,9 @@ export class HomeComponent implements OnInit {
   }
 
   onRegisterClick() {
+    //How read values from environment, TODO - remove it
+    console.log("LOCAL API URL: " + environment.localApiUrl)
+    console.log("USER API URL: " + environment.userApiUrl)
     this.router.navigate(['register']);
   }
 
