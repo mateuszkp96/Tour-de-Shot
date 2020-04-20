7abd2e1c207aa3f56d3faecaac0b0c383e6c0574
Mateusz Kopeć <mateuszkp96@gmail.com>
2020-04-20 20:17:53 +0200

    Reading endpoints via environment variables for docker (#51)

---
+++ ui/src/assets/env.js
@@ -0,0 +1,8 @@
+(function (window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["localApiUrl"] = "localhost:80/api/local";
+  window["env"]["userApiUrl"] = "localhost:80/api/user";
+  window["env"]["debug"] = true
+})(this);
