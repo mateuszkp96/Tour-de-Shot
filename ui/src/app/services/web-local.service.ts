1bee69250997c149c63fc8db725ef05c7fe54bb5
Aleksandra Zwierzchowska <aazwierzchowska@gmail.com>
2020-04-23 16:14:47 +0200

    Changes in weblocalservice

--- ui/src/app/services/web-local.service.ts
+++ ui/src/app/services/web-local.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { Subject, Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {Subject, Observable} from 'rxjs';
+import {env} from '../../assets/env';
+import {RequestOptions, Request, Headers, Http} from '@angular/http';
+import {serverAddress} from '../../assets/server.constant';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +11,21 @@ import { Subject, Observable } from 'rxjs';
 
 export class WebLocalService {
 
+  readonly LOCAL_API_URL;
   readonly ROOT_URL;
-  
+
   constructor(private http: HttpClient) {
     this.ROOT_URL = 'http://localhost:4200/assets/locals-db.json';
+    this.LOCAL_API_URL = env.localApiUrl;
   }
 
 
-  get(){
-    return this.http.get(`${this.ROOT_URL}`);
+  get() {
+    console.log("url")
+    console.log(this.LOCAL_API_URL + '/local/1')
+    return this.http.get(this.LOCAL_API_URL + '/local/1');
+    //return this.http.get(`${this.LOCAL_API_URL}`);
   }
-  
+
+
 }
--- ui/src/assets/server.constant.ts
+++ ui/src/assets/server.constant.ts
@@ -1,1 +1,4 @@
+import {env} from '../assets/env';
+//export const serverAddress = env.localApiUrl;
+//export const serverAddress = 'localhost:80/api/local/local/1';
 export const serverAddress = 'http://localhost:3000';
