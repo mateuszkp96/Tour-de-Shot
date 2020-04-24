56b0073dbf60587e48568ac9b90f6f4001371c50
Mateusz Kopeć <mateuszkp96@gmail.com>
2020-04-24 21:40:52 +0200

    Cors enabled for core app (#59)

--- ui/src/assets/env.js
+++ ui/src/assets/env.js
@@ -2,7 +2,7 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["localApiUrl"] = "localhost:80/api/local";
-  window["env"]["userApiUrl"] = "localhost:80/api/user";
+  window["env"]["localApiUrl"] = "http://localhost:80/api/local";
+  window["env"]["userApiUrl"] = "http://localhost:80/api/user";
   window["env"]["debug"] = true
 })(this);
