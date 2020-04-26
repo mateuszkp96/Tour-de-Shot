2f72ceba5a37b0d35b8badaf308b050e0fa23fde
Aleksandra Zwierzchowska <aazwierzchowska@gmail.com>
2020-04-26 13:53:14 +0200

    Cleaning proxy

--- ui/src/assets/server.constant.ts
+++ ui/src/assets/server.constant.ts
@@ -1,4 +1,1 @@
-import {env} from '../assets/env';
-//export const serverAddress = env.localApiUrl;
-//export const serverAddress = 'localhost:80/api/local/local/1';
 export const serverAddress = 'http://localhost:3000';
