1b36b3e2468b2cba42566654c9cf5be9f545c998
olabudzyn <ola.budz96@gmail.com>
2020-04-28 20:48:33 +0200

    adding default values in findAllLocalsPageable in controller, refactoring findAllPageable in repository (#67)

--- app/core/src/main/java/com/teamg/tourdeshot/core/repository/mongo/MongoLocalRepository.java
+++ app/core/src/main/java/com/teamg/tourdeshot/core/repository/mongo/MongoLocalRepository.java
@@ -7,10 +7,10 @@ import com.teamg.tourdeshot.core.repository.crud.delete.DeleteOperationResult;
 import com.teamg.tourdeshot.core.repository.crud.delete.DeleteResultInterpreter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -39,9 +39,10 @@ public class MongoLocalRepository implements LocalRepository {
     public Page<Local> findAllPageable(Pageable pageable) {
         Query query = new Query().with(pageable);
         List<Local> localList = mongoOperations.find(query, Local.class);
-        long count = mongoOperations.count(query, Local.class);
-        Page<Local> resultPage = new PageImpl<>(localList , pageable, count);
-        return resultPage;
+        return PageableExecutionUtils.getPage(
+                localList,
+                pageable,
+                () -> mongoOperations.count(Query.of(query).limit(-1).skip(-1), Local.class));
     }
 
     @Override
