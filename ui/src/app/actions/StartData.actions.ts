b4c7a433108640f52a08118b3b46515d4b091693
azwierzc <51276581+azwierzc@users.noreply.github.com>
2020-04-30 18:38:42 +0200

    Storing search data state (#68)
    * Storing start data state after refresh
    
    * Storing start data state after refresh
    
    * Deleting utc_offset error
    
    * OnDestroy changes

---
+++ ui/src/app/actions/StartData.actions.ts
@@ -0,0 +1,19 @@
+import { StartData } from './../models/StartData'
+
+export class AddStartData {
+  static readonly type = '[StartData] Add'
+
+  constructor(public payload: StartData) {}
+}
+
+export class RemoveStartData {
+  static readonly type = '[StartData] Remove'
+
+  constructor(public payload: number) {}
+}
+
+export class RemoveAllStartData {
+  static readonly type = '[StartData] AllRemove'
+
+  constructor() {}
+}
