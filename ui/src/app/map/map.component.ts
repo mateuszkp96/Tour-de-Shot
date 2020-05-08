8a5eb2d12445890b07cf8ddc7c1d59ddcd2ae341
Aleksandra Zwierzchowska <aazwierzchowska@gmail.com>
2020-05-09 00:40:47 +0200

    Getting locals from backend

--- ui/src/app/map/map.component.ts
+++ ui/src/app/map/map.component.ts
@@ -3,8 +3,9 @@ import {MapsAPILoader, MouseEvent} from '@agm/core';
 import {Local} from '../models/Local';
 import {Coordinates} from '../models/Coordinates';
 import {Subject, BehaviorSubject} from 'rxjs';
-import { LocalService } from '../services/local.service';
-import { StartPointService } from '../services/start-point.service';
+import {LocalService} from '../services/local.service';
+import {StartPointService} from '../services/start-point.service';
+import {WebLocalService} from '../services/web-local.service';
 
 @Component({
   selector: 'app-map',
@@ -15,21 +16,22 @@ import { StartPointService } from '../services/start-point.service';
 export class MapComponent implements OnInit, AfterViewInit {
 
   @Input() btnSearchClicked: Subject<any>;
-  @Input() currentCheckedLocalsIdList: BehaviorSubject<any>;
-  @Input() currentFilteredByDistLocalsList: BehaviorSubject<any>;
   @Input() localsList: Local[];
   @Input() filteredByDistLocalsList: Local[];
-  @Input() checkedLocalsIdList: number[]=[];
+  @Input() checkedLocalsIdList: number[] = [];
   @ViewChild("mapContainer", {static: false}) gmap: ElementRef;
 
 
   map: google.maps.Map;
   markers: google.maps.Marker[] = [];
   marker: google.maps.Marker;
+  markersId: number[] = [];
+  filteredByDistLocalsListId: number[] = [];
   autocomplete: google.maps.places.Autocomplete;
   place: google.maps.places.PlaceResult;
   private geoCoder;
-
+  destinations: google.maps.LatLng[] = [];
+  destinationsId: number[] = [];
   //start map center
   localizationLat: number = 52.229676;
   localizationLong: number = 21.012229;
@@ -37,22 +39,25 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   mapOptions: google.maps.MapOptions = {
     center: this.localizationCoordinates,
-    zoom: 12
+    zoom: 12.7
   };
 
   localIcon = {
     url: "../../assets/beer.png",
-    scaledSize: new google.maps.Size(40, 40),
+    scaledSize: new google.maps.Size(34, 34),
+    anchor: new google.maps.Point(17, 30)
   };
 
   checkedLocalIcon = {
     url: "../../assets/checkedBeer.png",
-    scaledSize: new google.maps.Size(40, 40),
+    scaledSize: new google.maps.Size(34, 34),
+    anchor: new google.maps.Point(17, 30)
   };
 
   localizationIcon = {
     url: "../../assets/localization.png",
-    scaledSize: new google.maps.Size(40, 40)
+    scaledSize: new google.maps.Size(34, 34),
+    anchor: new google.maps.Point(17, 30)
   };
 
   localizationMarker = new google.maps.Marker({
@@ -61,51 +66,58 @@ export class MapComponent implements OnInit, AfterViewInit {
     icon: this.localizationIcon
   });
 
+  routePolyline = new google.maps.Polyline({
+    path: this.destinations,
+    geodesic: true,
+    strokeColor: '#1a3fff',
+    strokeOpacity: 0.8,
+    strokeWeight: 4.5
+  });
+
 
   constructor(
     private ngZone: NgZone,
     private localService: LocalService,
-    private startPointService: StartPointService,
+    private webLocalService: WebLocalService,
+    private startPointService: StartPointService
   ) {
     this.startPointService.getStartPoint()
       .subscribe(mymessage => {
-        if(mymessage) {
+        if (mymessage) {
           this.localizationCoordinates = mymessage;
 
           this.localizationMarker.setPosition(this.localizationCoordinates);
           this.mapOptions.center = this.localizationCoordinates;
-          this.map.setCenter(this.localizationCoordinates);
+          this.map.setOptions(this.mapOptions)
           this.localizationMarker.setMap(this.map);
-
-          this.saveFilteredLocalsAsMarkers();
-          this.loadMarkers();
-        }});
+        }
+      });
 
     this.localService.getFilteredByDistLocalsList()
       .subscribe(mymessage => {
         this.filteredByDistLocalsList = mymessage;
         this.saveFilteredLocalsAsMarkers();
-        this.loadMarkers();
       });
+
 
     this.localService.getCheckedLocalsIdList()
       .subscribe(mymessage => {
         this.checkedLocalsIdList = mymessage;
         this.loadCheckedLocalsMarkers();
+        this.drawRoute();
       });
 
   }
 
 
   ngOnInit() {
-    console.log("map is load")
-
     this.geoCoder = new google.maps.Geocoder;
-
-   // this.localizationCoordinates = this.startPointService.getStartPointValue();
+    // this.localizationCoordinates = this.startPointService.getStartPointValue();
   }
-  ngAfterViewInit(){
+
+  ngAfterViewInit() {
     this.mapInitializer();
+
   }
 
 
@@ -121,8 +133,6 @@ export class MapComponent implements OnInit, AfterViewInit {
     this.mapOptions.center = this.localizationCoordinates;
     this.map.setCenter(this.localizationCoordinates);
 
-    //this.saveLocalsAsMarkers();
-    //this.loadMarkers();
   }
 
   saveLocalsAsMarkers() {
@@ -143,27 +153,41 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   saveFilteredLocalsAsMarkers() {
-
+    this.filteredByDistLocalsListId = [];
     if (this.filteredByDistLocalsList) {
-      this.clearMarkers();
-      this.markers = [];
-
       this.filteredByDistLocalsList.forEach(element => {
-        this.marker = new google.maps.Marker({
-          position: new google.maps.LatLng(element.coordinates.lat, element.coordinates.lon),
-          map: this.map,
-          title: element.name,
-          icon: this.localIcon,
-        });
-
-        this.marker.set("id", element.id);
-        this.markers.push(this.marker);
+        this.filteredByDistLocalsListId.push(element.id)
       });
 
+      this.filteredByDistLocalsList.forEach(element => {
+
+        // markers that must be added
+        if (!this.markersId.includes(element.id)) {
+          this.marker = new google.maps.Marker({
+            position: new google.maps.LatLng(element.coordinates.lat, element.coordinates.lon),
+            map: this.map,
+            title: element.name,
+            icon: this.localIcon,
+          });
+
+          this.marker.set("id", element.id);
+          this.markers.push(this.marker);
+          this.markersId.push(element.id);
+        }
+      });
+
+
+      // markers that must be deleted from map
+      let missing = this.markersId.filter(item => this.filteredByDistLocalsListId.indexOf(item) < 0);
+      missing.forEach(element => {
+        const index = this.markersId.indexOf(element, 0);
+        this.markers[index].setMap(null)
+        this.markersId.splice(index, 1);
+        this.markers.splice(index, 1);
+      });
     }
 
   }
-
 
   loadMarkers(): void {
     this.markers.forEach(markerInfo => {
@@ -186,6 +210,8 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   loadCheckedLocalsMarkers() {
+    console.log("Checked Locals Id List")
+    console.log(this.checkedLocalsIdList)
     for (let j in this.markers) {
       this.markers[j].setIcon(this.localIcon);
       this.markers[j].setMap(this.map);
@@ -195,11 +221,43 @@ export class MapComponent implements OnInit, AfterViewInit {
         if (this.markers[j].get("id") == i) {
           this.markers[j].setIcon(this.checkedLocalIcon);
           this.markers[j].setMap(this.map);
+
         }
       }
     }
   }
 
+  async drawRoute() {
+    if (this.checkedLocalsIdList) {
+      for (let i of this.checkedLocalsIdList) {
+
+        // destinations that must be added
+        if (!this.destinationsId.includes(i)) {
+          await this.webLocalService.getLocalById(i).then(local => {
+            this.destinationsId.push(local.id)
+            this.destinations.push(new google.maps.LatLng(local.coordinates.lat, local.coordinates.lon))
+
+            this.routePolyline.setPath(this.destinations)
+            this.routePolyline.setMap(this.map);
+
+          });
+        }
+      }
+
+      // destinations that must be deleted
+      let missing = this.destinationsId.filter(item => this.checkedLocalsIdList.indexOf(item) < 0);
+      missing.forEach(element => {
+        const index = this.destinationsId.indexOf(element, 0);
+        this.destinationsId.splice(index, 1);
+        this.destinations.splice(index, 1);
+        this.routePolyline.setPath(this.destinations);
+      });
+
+    }
+
+    console.log("Route Id List")
+    console.log(this.destinationsId)
+  }
 
 
 }
