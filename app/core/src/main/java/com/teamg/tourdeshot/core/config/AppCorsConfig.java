f433c162f365a0fb4d7effa6a3f180686c5faec1
Mateusz Kopeć <mateuszkp96@gmail.com>
2020-05-10 18:14:14 +0200

    Configureable cors origins in core (#75)

---
+++ app/core/src/main/java/com/teamg/tourdeshot/core/config/AppCorsConfig.java
@@ -0,0 +1,17 @@
+package com.teamg.tourdeshot.core.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "app.cors", ignoreUnknownFields = false)
+public class AppCorsConfig {
+    private final List<String> allowedOrigins = new ArrayList<>();
+}
--- app/core/src/main/java/com/teamg/tourdeshot/core/config/CorsConfig.java
+++ app/core/src/main/java/com/teamg/tourdeshot/core/config/CorsConfig.java
@@ -1,19 +1,25 @@
 package com.teamg.tourdeshot.core.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("dev")
 public class CorsConfig implements WebMvcConfigurer {
+
+    private final AppCorsConfig appCorsConfig;
+
+    @Autowired
+    public CorsConfig(AppCorsConfig appCorsConfig) {
+        this.appCorsConfig = appCorsConfig;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST","PUT", "DELETE", "HEAD")
+                .allowedOrigins(appCorsConfig.getAllowedOrigins().toArray(new String[0]))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
 }
