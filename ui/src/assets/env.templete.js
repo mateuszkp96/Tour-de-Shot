c6cf17295eca9c35f4cb5ddf5d47fc0b9738ce6a
Mateusz <mateuszkp96@gmail.com>
2020-05-10 12:32:42 +0200

    Google auth provider in env

--- ui/src/assets/env.templete.js
+++ ui/src/assets/env.templete.js
@@ -4,5 +4,6 @@
   // Environment variables
   window["env"]["localApiUrl"] = "${LOCAL_API_URL}";
   window["env"]["userApiUrl"] = "${USER_API_URL}";
+  window["env"]["googleAuthProvider"] = "${GOOGLE_AUTH_PROVIDER}";
   window["env"]["debug"] = "${DEBUG}";
 })(this);
