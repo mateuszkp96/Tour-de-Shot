ff87c301ac4b6a2416f510d262216c18053e9be1
Mateusz <mateuszkp96@gmail.com>
2020-05-25 22:11:38 +0200

    Userhistory microservice initial setup

---
+++ app/userhistory/devrun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mongoContainerName="devmongo"
+mongoContainerPort="27017"
+port_mapping="8080:8080"
+mongoNetwork="mongo_core_network"
+
+docker run --rm \
+  -p "${port_mapping}" \
+  --network="${mongoNetwork}" \
+  -e SPRING_DATA_MONGODB_HOST="${mongoContainerName}" \
+  -e SPRING_DATA_MONGODB_PORT="${mongoContainerPort}" \
+  -t tourdeshot/userhistory
