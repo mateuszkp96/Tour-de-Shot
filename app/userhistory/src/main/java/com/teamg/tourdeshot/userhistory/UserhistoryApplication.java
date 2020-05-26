8ce1b9dfc0a52fa3a08779ab8dd48142c59af0d9
Mateusz Kopeć <mateuszkp96@gmail.com>
2020-05-26 21:00:03 +0200

    Userhistory microservice initial setup (#77)

---
+++ app/userhistory/src/main/java/com/teamg/tourdeshot/userhistory/UserhistoryApplication.java
@@ -0,0 +1,13 @@
+package com.teamg.tourdeshot.userhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserhistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserhistoryApplication.class, args);
+    }
+
+}
