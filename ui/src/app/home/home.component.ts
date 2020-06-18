33fad903d4530a7acdae1cfa8d80a23a9a53d0e5
boxi01 <45149847+boxi01@users.noreply.github.com>
2020-06-18 17:29:32 +0200

    Checkbox with age confirmation (#83)

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { environment } from "../../environments/environment"
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
     private router: Router,
 
   ) {}
-  
+
   ngOnInit(): void {
   }
 
