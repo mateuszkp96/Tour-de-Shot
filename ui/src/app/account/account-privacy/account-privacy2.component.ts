b6d8b03e5277fae612cc7f2a6fc2898c8d51a3bb
boxi01 <joannapudelko95@gmail.com>
2020-06-22 18:41:31 +0200

    Style corrections

---
+++ ui/src/app/account/account-privacy/account-privacy2.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService, SocialUser } from 'angularx-social-login';
+
+@Component({
+  selector: 'app-account-privacy2',
+  templateUrl: './account-privacy2.component.html',
+})
+export class AccountPrivacyComponent2 implements OnInit {
+
+  public user: SocialUser;
+  public loggedIn: boolean;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+      this.loggedIn = (user != null);
+    });
+  }
+}
