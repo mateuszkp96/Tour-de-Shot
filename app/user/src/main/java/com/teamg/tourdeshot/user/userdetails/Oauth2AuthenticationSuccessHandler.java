b931e19ee3d9092edb390ad6a18d6627cb2211c6
Mateusz <mateuszkp96@gmail.com>
2020-06-23 13:16:28 +0200

    configureable redirect url

--- app/user/src/main/java/com/teamg/tourdeshot/user/userdetails/Oauth2AuthenticationSuccessHandler.java
+++ app/user/src/main/java/com/teamg/tourdeshot/user/userdetails/Oauth2AuthenticationSuccessHandler.java
@@ -4,7 +4,8 @@ package com.teamg.tourdeshot.user.userdetails;
 import com.teamg.tourdeshot.user.model.UserOAuth2Dto;
 import com.teamg.tourdeshot.user.service.UserRegistrationService;
 import com.teamg.tourdeshot.user.service.UserService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.RedirectStrategy;
@@ -14,17 +15,30 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 @Configuration("oauth2authSuccessHandler")
-@RequiredArgsConstructor
 public class Oauth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
 
     private  final UserService userService;
 	private final UserRegistrationService userRegistrationService;
 	private final RedirectStrategy redirectStrategy;
-	
-	@Override
+	private final String redirectUrl;
+
+	@Autowired
+    public Oauth2AuthenticationSuccessHandler(UserService userService,
+                                              UserRegistrationService userRegistrationService,
+                                              RedirectStrategy redirectStrategy,
+                                              @Value("${app.url.redirect}") String redirectUrl) {
+        this.userService = userService;
+        this.userRegistrationService = userRegistrationService;
+        this.redirectStrategy = redirectStrategy;
+        this.redirectUrl = Objects.requireNonNullElse(redirectUrl, "http://google.com");
+    }
+
+
+    @Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
 		if(!this.userService.userCreated(authentication.getName())) {
@@ -32,7 +46,7 @@ public class Oauth2AuthenticationSuccessHandler implements AuthenticationSuccess
 			UserOAuth2Dto user = new UserOAuth2Dto(principal.getFirstName(), principal.getLastName(), authentication.getName(), principal.getEmail());
 			this.userRegistrationService.registerNewAuth2User(user);
 		}
-		 this.redirectStrategy.sendRedirect(request, response, "http://34.105.178.183.xip.io/");
+		 this.redirectStrategy.sendRedirect(request, response, this.redirectUrl);
 	}
 	
 
