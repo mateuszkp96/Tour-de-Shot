8a622801fe55e7781caac40f1a92e6ac124227d9
Mateusz <mateuszkp96@gmail.com>
2020-06-23 20:32:57 +0200

    AAA

--- ui/src/app/services/web-login.service.ts
+++ ui/src/app/services/web-login.service.ts
@@ -13,19 +13,7 @@ export class WebLoginService {
   constructor(private http: HttpClient) {}
 
    async getUserJson(): Promise<any> {
-    return await this.http.get(environment.keycloakLoginUrl, {responseType: 'text'}).subscribe(
-      (res: any) => {
-        alert("News Success");
-        // Where you find the array res.data or res.data.data
-        console.log('res is ', res);
-      },
-      error => {
-        alert("ERROR");
-      });
-   }
-
-
-    )
+    return await this.http.get(environment.keycloakLoginUrl, {responseType: 'text'}).toPromise()
     //return await this.http.get(this.ROOT_URL).toPromise()
   }
 
