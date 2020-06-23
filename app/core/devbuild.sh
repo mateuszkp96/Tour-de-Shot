196109b609f092950901f072383a3ef5917fd920
Mateusz <mateuszkp96@gmail.com>
2020-06-23 12:57:14 +0200

    A

--- app/core/devbuild.sh
+++ app/core/devbuild.sh
@@ -2,4 +2,4 @@
 
 ./gradlew build
 mkdir -p build/dependency && (cd build/dependency; jar -xf ../libs/*.jar)
-docker build -t mateusz43211/tourdeshot:core_test1.02 --no-cache .
+docker build -t tourdeshot/core .
