024ba87174d8bc28ed283f9c347906be94a74537
Mateusz Kopeć <mateuszkp96@gmail.com>
2020-06-24 18:53:31 +0200

    bearer in authentication header (#121)

--- ui/src/app/jwt.interceptor.ts
+++ ui/src/app/jwt.interceptor.ts
@@ -32,7 +32,7 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
-        Authorization: `${this.idToken}`
+        Authorization: `Bearer ${this.idToken}`
       },
       withCredentials: true
     });
